53a5b530f8e8d47766d20153ecca62884adc7767
Tran Nguyen Hao Minh <Minh.trannguyen@nfcdtadmin.local>
2019-01-28 11:04:20 +0800

    fix bug for duplicated enum values

--- loaders/mysql.go
+++ loaders/mysql.go
@@ -242,11 +242,11 @@ switchDT:
 }
 
 // MyEnumValues loads the enum values.
-func MyEnumValues(db models.XODB, schema string, enum string) ([]*models.EnumValue, error) {
+func MyEnumValues(db models.XODB, schema string, tableName string, enum string) ([]*models.EnumValue, error) {
 	var err error
 
 	// load enum vals
-	res, err := models.MyEnumValues(db, schema, enum)
+	res, err := models.MyEnumValues(db, schema, tableName, enum)
 	if err != nil {
 		return nil, err
 	}
